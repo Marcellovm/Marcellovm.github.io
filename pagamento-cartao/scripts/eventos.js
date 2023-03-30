d48770880514cb7920a06160b31f71a553b13cae
marcellovm <marcello@TEPLACONTABIL.LOCAL>
2023-03-30 17:42:57 -0300

    Iniciando os scripts card.js e eventos.js

---
+++ pagamento-cartao/scripts/eventos.js
@@ -0,0 +1,21 @@
+let meses = [ 
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+];
+for (let m = 0; m <= 11; m++) {
+    mes.innerHTML += "<option>" + meses[m] + "</option>"; 
+}
+for (let a = 2023; a <= 2043; a++) {
+    let value = a.toString().substr(2, 2);
+    ano.innerHTML += `<option value="${value}">${a}</option>`;
+ }
